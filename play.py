08c08d54bd0e0f7f0639dc3a2864cacc0d0d75b0
Benjamin Dupuis <dupuis23@hotmail.com>
2018-09-22 14:26:23 -0400

    Update comments

--- play.py
+++ play.py
@@ -3,25 +3,25 @@ import pygame
 import time
 
 
-pygame.init()   # intializes the pygame
+pygame.init()   # Intialize the game
 snake = Snake()
 apple = Apple()
-env = Environment(screen_width=400, screen_height=400)   # makes the screen larger so it's easier to play
+env = Environment(screen_width=400, screen_height=400)   # Make the screen larger so it's easier to play
 running = True
 done = True
-action = 0  # initialize the action to be 0 (LEFT)
+action = 0  # Initialize the action to be 0 (LEFT)
 
 while running:
-    time.sleep(0.08)   # to make the game slow down
+    time.sleep(0.08)   # Make the game slow down
 
-    if done:   # game over, start a new game
+    if done:   # Game over, start a new game
         time.sleep(1)
         env.reset()
         action = 0
 
     env.render(display=True)
 
-    _, _, done = env.step(action)  # make the snake move according to the chosen action
+    _, _, done = env.step(action)  # Make the snake move according to the chosen action
 
     for event in pygame.event.get():
         if event.type == pygame.KEYDOWN:
